b5c5906a5e095e2267f6f653b55f1836bc077777
Fl0riann <flooo.meissonnier@gmail.com>
2024-06-20 11:24:19 +0200

    Envoie d'une requête refresh consultation son toutes les secondes

--- consultation_son.php
+++ consultation_son.php
@@ -122,14 +122,16 @@
   ?>
 
   <script>
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
-        }
-    };
-    xmlhttp.open("GET", "./consultation_son_refresh.php?salle=<?php echo $salle_selectionnee; ?>", true);
-    xmlhttp.send();
+    setTimeout(function() {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.responseText);
+            }
+        };
+        xmlhttp.open("GET", "./consultation_son_refresh.php?salle=<?php echo $salle_selectionnee; ?>", true);
+        xmlhttp.send();
+    }, 1000)
 </script>
 
 
